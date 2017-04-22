eaf545db3872c0ac642c86a50c62a8459133a80c
surr <litichevskij.vova@gmail.com>
2017-04-22 17:41:02 +0300

    + jsdocs

--- bin/findSubsetIncludingSubstringFromWordStart/index.js
+++ bin/findSubsetIncludingSubstringFromWordStart/index.js
@@ -2,6 +2,21 @@
 
 module.exports = findSubsetBorders;
 
+/**
+ * at sorted array of unique strings
+ * finds subset of elements
+ * which starts from the given substring
+ * or where the given substring can be inserted to
+ *
+ * @param {String}   str - the substring
+ * @param {String[]} set - the sorted array of unique strings
+ * @returns {Object} res - result
+ * @returns {Number} res.length - length of the found subset
+ * @returns {Number} res.start - index where the subset starts,
+ *                                  or where the substring can be inserted to
+ *                                  if the res.length === 0
+ * TODO check the 'from' and 'to' params work properly
+ */
 function findSubsetBorders ( str, set, from = 0, to = (set.length - 1) ) {
     const strl = str.length;
     let left    = leftBorder(set, from, to, str, strl),
@@ -16,6 +31,16 @@ function findSubsetBorders ( str, set, from = 0, to = (set.length - 1) ) {
     return { start: left, length: rightBorder(set, left, to, str, strl) - left + 1 }
 }
 
+/**
+ * to find left border of the subset
+ *
+ * @param {String[]} set  - the sorted array of unique strings
+ * @param {Number}   min  - from where to search
+ * @param {Number}   max  - until where to search
+ * @param {String}   str  - the substring
+ * @param {Number}   strl - length of the substring
+ * @returns {Number} index of the left border
+ */
 function leftBorder ( set, min, max, str, strl) {
     let i,
         _max = ++max;
@@ -31,6 +56,16 @@ function leftBorder ( set, min, max, str, strl) {
     return max === _max ? min : max; // min if max is never reached
 }
 
+/**
+ * to find right border of the subset
+ *
+ * @param {String[]} set  - the sorted array of unique strings
+ * @param {Number}   min  - from where to search
+ * @param {Number}   max  - until where to search
+ * @param {String}   str  - the substring
+ * @param {Number}   strl - length of the substring
+ * @returns {Number} index of the right border
+ */
 function rightBorder ( set, min, max, str, strl) {
     let i;
 
@@ -51,6 +86,17 @@ function bisect ( min, max ) {
     return diff < 2 ? undefined : min + ( diff>>1 )
 }
 
+/**
+ * compare two strings
+ *
+ * @param {String} a
+ * @param {String} b
+ * @param {Number} bLength - length of the b
+ * @param {Boolean} cutA   - to truncate the a to length of b or not
+ * @returns {Number} res   -    0  if a === b
+ *                              -1 if a < b
+ *                               1 if a > b
+ */
 function compare ( a, b, bLength, cutA = true ) {
     if ( cutA ) a = a.substring(0, bLength);
 
